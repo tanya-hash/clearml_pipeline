6c61fb8eaaf9d2245bd33ba17e959df1ab44fb55
Tanya Jain <75388332+tanya-hash@users.noreply.github.com>
2023-11-15 13:02:21 +0530

    Update clearml_pipeline.py

--- clearml_pipeline.py
+++ clearml_pipeline.py
@@ -63,17 +63,29 @@ def preprocessing():
 
     plot = sns.countplot(train.Gender)
     plt.show(block=False)
-    Logger.current_logger().report_matplotlib_figure("Gender and Response",plot,report_image=False,report_interactive=True)
+    Logger.current_logger().report_matplotlib_figure(title="Gender and Reponse",
+    series="ignored",
+    figure=plot,
+    report_interactive=True,
+)
     
     df = train.groupby(['Vehicle_Age','Response'])['id'].count().to_frame().rename(columns={'id':'count'}).reset_index()
     plot = sns.catplot(x="Vehicle_Age", y="count",col="Response",data=df, kind="bar",height=4, aspect=.7)
     plt.show(block=False)
-    Logger.current_logger().report_matplotlib_figure("Vehicle Age and Response",plot,report_image=False,report_interactive=True)
+    Logger.current_logger().report_matplotlib_figure(title="Vehicle Age and Reponse",
+    series="ignored",
+    figure=plot,
+    report_interactive=True,
+)
     
     df = train.groupby(['Vehicle_Damage','Response'])['id'].count().to_frame().rename(columns={'id':'count'}).reset_index()
     plot = sns.catplot(x="Vehicle_Damage", y="count",col="Response", data=df, kind="bar",height=4, aspect=.7)
     plt.show(block=False)
-    Logger.current_logger().report_matplotlib_figure("Vehicle Damage and Response",plot,report_image=False,report_interactive=True)
+    Logger.current_logger().report_matplotlib_figure(title="Vehicle Damage and Response",
+    series="ignored",
+    figure=plot,
+    report_interactive=True,
+)
 
 
     train['Gender'] = train['Gender'].map({'Female': 0, 'Male': 1}).astype(int)
